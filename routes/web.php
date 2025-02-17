88ef5bfde435ae7fb8431917abe578fe53da30be
Dhiya' Ulhaq Ulayya <dhiyaulul108@gmail.com>
2025-02-17 14:52:27 +0700

    Modifikasi_Praktikum3_Job2

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,6 @@ Route::get('/user/{name?}', function ($name='John') {
 });
 
 Route::resource('photos', PhotoController::class); 
+
+Route::get('/greeting', [WelcomeController::class,'greeting']);
+
