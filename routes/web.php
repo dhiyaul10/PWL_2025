8eb811a5e35c974ce759fdc22c5a42b3b9a05be5
Dhiya' Ulhaq Ulayya <dhiyaulul108@gmail.com>
2025-02-17 10:33:36 +0700

    Modifikasi_Praktikum1_Job2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+Route::get('/hello', function () {
+    return 'Hello, World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return '2341720021_Dhiya';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Dhiya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Dhiya '.$name;
 });
